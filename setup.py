320b37b2e0194242b4b90aaecce85f3d31ad3d7b
Chris Warth <cwarth@fhcrc.org>
2016-09-19 16:37:16 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(name='igdbweb',
       version='0.1.0',
       description='Immunoglobin germline web interface',
       author='Chris Warth <cwarth@fredhutch.org>',
-      packages=find_packages('igdbweb'),
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
       install_requires=[
           'flask','frozen-flask','numpy','biopython'
       ],
 
-      package_data={'igdbweb': ['templates/*']},
-      )
+      package_data={'igdbweb': ['templates/*']}
+)
